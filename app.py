3f584c9b4fe428953f8c1ed54741ba6377eb661a
AndreOliveiraMendes <ao_mendes@hotmail.com>
2025-12-12 14:54:26 -0300

    improved function

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
-#!usr/env python
-def to_regional_indicator(msg):
+#!/usr/bin/env python3
+import unicodedata
+
+def normalize_ascii(s: str) -> str:
+    return "".join(
+        c for c in unicodedata.normalize("NFD", s)
+        if unicodedata.category(c) != "Mn"
+    )
+
+def to_regional_indicator(msg: str) -> str:
+    msg = normalize_ascii(msg)
     res = []
-    for l in msg:
-        if l.isalpha():
-            res.append(f":regional_indicator_{l.lower()}:")
+    for ch in msg:
+        if ch.isalpha() and 'a' <= ch.lower() <= 'z':
+            res.append(f":regional_indicator_{ch.lower()}:")
+        elif ch:
+            res.append(ch)
         else:
-            res.append(l)
+            res.append('')
     return " ".join(res)
 
-msg = input("msg:")
-print(to_regional_indicator(msg))
+if __name__ == "__main__":
+    import sys
 
+    if len(sys.argv) > 1:
+        message = " ".join(sys.argv[1:])
+    else:
+        message = input("Message: ")
+
+    print(to_regional_indicator(message))
